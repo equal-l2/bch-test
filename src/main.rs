30a397dc6a3b47efe40c22ff50d3590750e1e14e
equal-l2 <eng.equall2@gmail.com>
2019-10-30 02:37:56 +0900

    Compute syndrome

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+
 // 長さlenで全てのビットが立ったビット列を生成する
 fn gen_mask(len: u8) -> u16 {
     let mut ret = 0;
@@ -15,7 +17,8 @@ fn gf2_mod(dividend: u16, divisor: u16, divisor_digits: u8) -> u16 {
 
     for i in (digits..16).rev() {
         // 上から1ビットずつずらしながら見ていく
-        if (dividend & (1 << i)) != 0 { // 最上位ビットが立っていれば
+        if (dividend & (1 << i)) != 0 {
+            // 最上位ビットが立っていれば
             let i_pad = i - digits;
             // 今見ている値と割る数のXORをとる
             let mask = mask_arch << i_pad;
@@ -38,14 +41,15 @@ fn bch_encode(input: u16, gen: u16, gen_len: u8) -> u16 {
 }
 
 // 符号ビット列からBCH(15, 11)のシンドロームを得る
-fn bch_decode(input: u16, gen: u16, gen_len:u8) -> u16 {
+fn bch_decode(input: u16, gen: u16, gen_len: u8) -> u16 {
     gf2_mod(input, gen, gen_len)
 }
 
 // in1とin2のハミング距離を計算する
 fn hamming_dist(in1: u16, in2: u16) -> u8 {
     let mut ret = 0;
-    for i in 0..16 { // 各ビットごとに比較
+    for i in 0..16 {
+        // 各ビットごとに比較
         let mask = 1 << i;
         if (in1 & mask) != (in2 & mask) {
             ret += 1;
@@ -57,31 +61,62 @@ fn hamming_dist(in1: u16, in2: u16) -> u8 {
 fn main() {
     let gen = 0b10011; // 生成多項式
     let gen_len = 5; // 生成多項式のビット数
+    let code_len = 15; // 符号長
+    let errors = 1; // 誤り訂正可能数
+
+    let data_len = code_len - gen_len + 1;
 
     // 全情報ビットパターンについて符号を生成
-    let mut codes = Vec::with_capacity(1<<11);
-    for i in 0b00000_00000_0..=0b11111_11111_1 {
+    let mut codes = Vec::with_capacity(1 << data_len);
+    for i in 0..(1 << data_len) {
         let code = bch_encode(i, gen, gen_len); // 符号生成
         assert_eq!(bch_decode(code, gen, gen_len), 0); // シンドロームが0か確認
         codes.push(code); // codesに格納
     }
 
-    // 相異なる符号間のハミング距離を計算
+    // 符号をファイルに保存
+    {
+        let mut f = std::fs::File::create("codes.txt").unwrap();
+        for i in codes.iter() {
+            writeln!(f, "{:0w$b}", i, w = code_len as usize).unwrap();
+        }
+    }
+
+    // 相異なる符号間のハミング距離の分布を調べる
     let mut dists = std::collections::BTreeMap::new();
     for i in 0..codes.len() {
         for j in (i + 1)..codes.len() {
             let dist = hamming_dist(codes[i], codes[j]);
-            if dists.contains_key(&dist) {
-                let handle = dists.get_mut(&dist).unwrap();
-                *handle += 1;
-            } else {
-                dists.insert(dist, 1);
-            }
+            *dists.entry(dist).or_insert(1) += 1;
         }
     }
 
-    // 計算結果を出力
-    for (k, v) in dists {
+    // ハミング距離の分布を出力
+    for (k, v) in dists.iter() {
         println!("{} : {}", k, v);
+    }
+
+    // シンドロームを生成
+    let mut syndromes = Vec::new();
+    for i in 0..(1 << code_len) {
+        if hamming_dist(0, i) <= errors {
+            syndromes.push((i, bch_decode(i, gen, gen_len)));
+        }
+    }
+
+    // シンドロームをファイルに保存
+    {
+        let mut f = std::fs::File::create("syndromes.txt").unwrap();
+        for (k, v) in syndromes {
+            writeln!(
+                f,
+                "{:0w1$b} : {:0w2$b}",
+                k,
+                v,
+                w1 = code_len as usize,
+                w2 = gen_len as usize - 1
+            )
+            .unwrap();
+        }
     }
 }
